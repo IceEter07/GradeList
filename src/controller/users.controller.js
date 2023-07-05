f302f5158a357e4274252f27ac04a6fab3791ede
IceEter08 <inv2378@innovaccion.mx>
2023-07-04 19:12:26 -0600

    Versión 4 del modulo editar usuario

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -15,30 +15,39 @@ userController.index = (req, res) => {
 userController.renderEditForm = async (req, res) =>{
     
     const user = await User.findById(req.params.id).lean()
-    res.render('users/editUser', {user})
+    res.render('layouts/index', {user: user,
+    template:{
+        path: 'users/editUser',
+        title: 'Edit User',
+        css: ['main','userEdit']
+    }})
 }
 
 userController.updateUser = async (req, res) =>{
-    const {name, ap1, ap2, email} = req.body
+    const {name, ap1, ap2} = req.body
 
-    const emailUser = await User.findOne({email: email})
+    await User.findByIdAndUpdate(req.params.id, {name, ap1, ap2})
+    console.log("User updated succesfully");
+    req.flash('success_msg', 'Los datos han sido actualizados correctamente');
+    res.redirect(`../../user/editForm/${req.params.id}`)
     
-    if(emailUser){
-        req.flash('error_msg', 'El correo ya esta en uso');
+    // const emailUser = await User.findOne({email: email})
+    // if(emailUser){
+    //     req.flash('error_msg', 'El correo ya esta en uso');
         
-        //La línea de abajo no se podía ejecutar porque el método put que se hacía desde el formulario
-        //hacia que entrara en una "subcarpeta" que no permitia que el redirect funcionara adecuadamente.
-        //SE RECOMIENDA REVISIÓN
-        res.redirect(`../../user/editForm/${req.params.id}`)
-    }
-    else{
-        //La línea req.params.id deberá cambiarse una vez que se tenga listo el login
-        await User.findByIdAndUpdate(req.params.id, {name, ap1, ap2, email})
-        console.log("User updated succesfully");
-        req.flash('success_msg', 'Los datos han sido actualizados correctamente');
-        res.redirect(`../../user/editForm/${req.params.id}`)
-    }
-    // res.redirect('/index')
+    //     //La línea de abajo no se podía ejecutar porque el método put que se hacía desde el formulario
+    //     //hacia que entrara en una "subcarpeta" que no permitia que el redirect funcionara adecuadamente.
+    //     //SE RECOMIENDA REVISIÓN
+    //     res.redirect(`../../user/editForm/${req.params.id}`)
+    // }
+    // else{
+    //     //La línea req.params.id deberá cambiarse una vez que se tenga listo el login
+    //     await User.findByIdAndUpdate(req.params.id, {name, ap1, ap2, email})
+    //     console.log("User updated succesfully");
+    //     req.flash('success_msg', 'Los datos han sido actualizados correctamente');
+    //     res.redirect(`../../user/editForm/${req.params.id}`)
+    // }
+    // // res.redirect('/index')
     
 }
 
