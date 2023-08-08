5cd154d8249babd8f7421bc99e11640a30e38e7e
IceEter07 <alberto.cuadros90@gmail.com>
2023-08-08 11:11:48 -0600

    Maquetación general de la funcionalidad principal

--- src/assets/js/emergente.js
+++ src/assets/js/emergente.js
@@ -1,18 +1,13 @@
 const emergenteDiv = document.querySelector("#emergente")
-// const obscurecerBody = document.getElementById("body");
-// emergenteDiv.style.display = "none";
 const cards = document.querySelector(".cards")
 
 function mostrar(e) {
     if (emergenteDiv.style.display == "none") {
         emergenteDiv.style.display = "flex";
         cards.style.filter = "blur(3px)"
-        // emergenteDiv.style.filter = "blur(0px)";
-        // obscurecerBody.style.filter = "blur(4px)"
 
     } else {
         emergenteDiv.style.display = "none"
-        // obscurecerBody.style.filter = "blur(0px)"
         cards.style.filter = "blur(0px)"
     }
     
