3bf0113166f6f7265b4f609cb918cb9cca71eafb
ArmentaCE <armentaemanuel1107@gmail.com>
2023-08-08 12:21:59 -0600

    Ventana modal de formulario para agregar instituciones

--- src/assets/js/emergente.js
+++ src/assets/js/emergente.js
@@ -1,16 +1,28 @@
 const emergenteDiv = document.querySelector("#emergente")
 const cards = document.querySelector(".cards")
+const institucionForm = document.querySelector("#institucionesForm")
 
 function mostrar(e) {
-    if (emergenteDiv.style.display == "none") {
-        emergenteDiv.style.display = "flex";
-        cards.style.filter = "blur(3px)"
-
-    } else {
-        emergenteDiv.style.display = "none"
-        cards.style.filter = "blur(0px)"
-    }
+    if (e.classList.contains('addGroup')) {
+        if (emergenteDiv.style.display == "none") {
+            emergenteDiv.style.display = "flex";
+            cards.style.filter = "blur(3px)"
     
+        } else {
+            emergenteDiv.style.display = "none"
+            cards.style.filter = "blur(0px)"
+        }
+    }
+    if (e.classList.contains('instituciones')) {
+        if (institucionForm.style.display == "none") {
+            institucionForm.style.display = "flex";
+            cards.style.filter = "blur(3px)"
+    
+        } else {
+            institucionForm.style.display = "none"
+            cards.style.filter = "blur(0px)"
+        }
+    }
 }
 
 function mostrarEditForm(e){
